3a0e63f95764f286c04dbad930c0c4245b3e4500
Todd Sundsted <todd@sundsted.com>
2009-05-23 20:33:42 -0400

    Encode textarea elements, too.

--- iui/iui.js
+++ iui/iui.js
@@ -407,6 +407,7 @@ function encodeForm(form)
     encode(form.getElementsByTagName("input"));
     encode(form.getElementsByTagName("textarea"));
     encode(form.getElementsByTagName("select"));
+    encode(form.getElementsByTagName("textarea"));
     return args;    
 }
 
